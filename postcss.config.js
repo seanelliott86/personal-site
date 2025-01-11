ba765f2f639ff0a2002116d94e2945e5dcd40c3e
Sean Elliott <hello@sean-elliott.net>
2025-01-11 21:51:55 +1100

    added process to minfied css

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,11 @@
 import postcssImport from 'postcss-import';
 import postcssCustomMedia from 'postcss-custom-media';
+import cssnano from 'cssnano';
 
 export default {
     plugins: [
         postcssImport,
         postcssCustomMedia,
+        ...(process.env.NODE_ENV === 'production' ? [cssnano({ preset: 'default' })] : []),
     ],
 };
